b875868959e4677f131973cd7ef7a29cf96c654b
bappy <bappy3a@gmail.com>
2025-08-28 13:33:37 +0600

    telwine setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Livewire Project</title>
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
+
 <body>
-    <h1 style="text-align: center;">Welcome to livewire project </h1>
-    
-    {{-- @livewire('post', ['name' => 'Sunno Bappy']) --}}
-
-    {{-- @livewire('category')
-    <br>
-    @livewire('category-list') --}}
-    {{-- <livewire:post/> --}}
+    <h1 class="text-3xl font-bold text-blue-600 text-center mt-8 mb-2.5">Welcome to livewire project </h1>
 
 
+    <form class="mx-auto columns-3 p-20">
+        <label for="small" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Small select</label>
+        <select id="small"
+            class="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option selected>Choose a country</option>
+            <option value="US">United States</option>
+            <option value="CA">Canada</option>
+            <option value="FR">France</option>
+            <option value="DE">Germany</option>
+        </select>
+        <label for="default" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Default select</label>
+        <select id="default"
+            class="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option selected>Choose a country</option>
+            <option value="US">United States</option>
+            <option value="CA">Canada</option>
+            <option value="FR">France</option>
+            <option value="DE">Germany</option>
+        </select>
+        <label for="large" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Large select</label>
+        <select id="large"
+            class="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option selected>Choose a country</option>
+            <option value="US">United States</option>
+            <option value="CA">Canada</option>
+            <option value="FR">France</option>
+            <option value="DE">Germany</option>
+        </select>
+    </form>
 
-    @livewire('category-select')
-    <livewire:sub-category-select />
-    
+
 
     @livewireScripts
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 </body>
+
 </html>
